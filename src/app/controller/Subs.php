a0f71bc0bbd7a6a2e63c42b8337b0e73a1dbd711
shidqizh <shidqizh@gmail.com>
2023-11-17 14:24:29 +0700

    connect soap

--- src/app/controller/Subs.php
+++ src/app/controller/Subs.php
@@ -1,9 +1,179 @@
 <?php
 
-class Subs extends Controller{
-    public function index(){
-        $this->view('Subs');
+class Subs extends Controller {
+    public function index() {
+        $ch = curl_init();
+        $url = "http://host.docker.internal:8001/subscription";
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, true);
+
+        $xmlRequest = '<Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
+        <Body>
+            <getAll xmlns="http://interfaces/">
+                <api_key xmlns="">PHP_API_KEY</api_key>
+            </getAll>
+        </Body>
+        </Envelope>';
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $xmlRequest);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: text/xml'));
+
+        $response = curl_exec($ch);
+        if ($response === false) {
+            $error = curl_error($ch);
+        }
+        curl_close($ch);
+
+        // Handling errors
+        if ($response === false) {
+            echo "Error: " . $error;
+            return;
+        }
+
+        $xmlResponse = simplexml_load_string($response);
+        $result = $xmlResponse->xpath('//return');
+        $resultText = "<ul>";
+        foreach ($result as $subscription) {
+            $creator_id = $subscription->creator_id;
+            $subscriber_id = $subscription->subscriber_id;
+            $status = $subscription->status;
+
+            $resultText .= "<li>Creator ID: $creator_id, Subscriber ID: $subscriber_id, Status: $status</li>";
+        }
+        $resultText .= "</ul>";
+
+        $data['resultText'] = $resultText;
+        
+
+        $this->view('Subs', $data);
     }
 
-}
+    public function accept() {
+        $creator_id = $_POST['creator_id'];
+        $subscriber_id = $_POST['subscriber_id'];
+        
+        $this->callSOAPAPIWithSubscription('acceptSubscription', $creator_id, $subscriber_id);
+    }
 
+    public function getAll() {
+        $this->callSOAPAPIWithoutSubscription('getAll');
+    }
+
+    public function getAllSubsByIds() {
+        $subscriber_id = $_POST['subscriber_id'];
+        
+        $this->callSOAPAPIWithSubscription('getAllSubsByIds', null, $subscriber_id);
+    }
+
+    public function reject() {
+        $creator_id = $_POST['creator_id'];
+        $subscriber_id = $_POST['subscriber_id'];
+
+        $this->callSOAPAPIWithSubscription('rejectSubscription', $creator_id, $subscriber_id);
+    }
+
+    public function insert() {
+        $creator_id = $_POST['creator_id'];
+        $subscriber_id = $_POST['subscriber_id'];
+
+        $this->callSOAPAPIWithSubscription('insertSubscription', $creator_id, $subscriber_id);
+    }
+
+    private function callSOAPAPIWithoutSubscription($method) {
+        $ch = curl_init();
+        $url = "http://host.docker.internal:8001/subscription";
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, true);
+    
+        $xmlRequest = '<Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
+            <Body>
+                <' . $method . ' xmlns="http://interfaces/">
+                    <api_key xmlns="">PHP_API_KEY</api_key>
+                </' . $method . '>
+            </Body>
+        </Envelope>';
+    
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $xmlRequest);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: text/xml'));
+    
+        $response = curl_exec($ch);
+        if ($response === false) {
+            $error = curl_error($ch);
+        }
+        curl_close($ch);
+    
+        if ($response === false) {
+            echo "Error: " . $error;
+            return;
+        }
+    
+        $xmlResponse = simplexml_load_string($response);
+        $result = $xmlResponse->xpath('//return');
+        $resultText = "<ul>";
+        foreach ($result as $subscription) {
+            $creator_id = $subscription->creator_id;
+            $subscriber_id = $subscription->subscriber_id;
+            $status = $subscription->status;
+    
+            $resultText .= "<li>Creator ID: $creator_id, Subscriber ID: $subscriber_id, Status: $status</li>";
+        }
+        $resultText .= "</ul>";            
+        $data['response'] = $resultText;
+        
+        $this->view('Subs', $data);
+    }
+    
+    private function callSOAPAPIWithSubscription($method, $creator_id, $subscriber_id) {
+        $ch = curl_init();
+        $url = "http://host.docker.internal:8001/subscription";
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, true);
+    
+        $xmlRequest = '<Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
+            <Body>
+                <' . $method . ' xmlns="http://interfaces/">
+                    
+                    <subscription xmlns="">
+                        <api_key>PHP_API_KEY</api_key>
+                        <creator_id>' . $creator_id . '</creator_id>
+                        <subscriber_id>' . $subscriber_id . '</subscriber_id>
+                    </subscription>
+                </' . $method . '>
+            </Body>
+        </Envelope>';
+    
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $xmlRequest);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: text/xml'));
+    
+        $response = curl_exec($ch);
+        if ($response === false) {
+            $error = curl_error($ch);
+        }
+        curl_close($ch);
+    
+        if ($response === false) {
+            echo "Error: " . $error;
+            return;
+        }
+    
+        $xmlResponse = simplexml_load_string($response);
+        $result = $xmlResponse->xpath('//return');
+        $resultText = "<ul>";
+        foreach ($result as $subscription) {
+            $creator_id = $subscription->creator_id;
+            $subscriber_id = $subscription->subscriber_id;
+            $status = $subscription->status;
+    
+            $resultText .= "<li>Creator ID: $creator_id, Subscriber ID: $subscriber_id, Status: $status</li>";
+        }
+        $resultText .= "</ul>";            
+        $data['response'] = $resultText;
+        
+        $this->view('Subs', $data);
+    }
+    
+
+    
+}
--- src/app/view/Subs.php
+++ src/app/view/Subs.php
@@ -6,11 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     <link rel ="stylesheet" type="text/css" href="/public/css/Album.css">
-
     <title>Subs</title>
+    
 </head>
 <body>
-    <div class="navbar">
+<div class="navbar">
         <div class="logo">
             <a href="<? BASEURL ?>/public/home/index/1">
                 <img src="../../../public/images/logo.png" alt="">
@@ -31,49 +31,99 @@
             </div>
         </div>
         <div class="search_bar">
-            <form id="searchForm">
+            <form action="#" method="get">
 
-                <div class="search-sort">
-                    <div class="search-box">
-                        <i class="bi bi-search"></i>
-                        <button type="submit"><img src="../../../public/img/search.svg" alt=""></button>
-                        <input id="searchInput" type="text" name = "song_title" placeholder="Search...">
-                    </div>
-                    <div class = "filter">
-                        <select name="filter-genre" id="filter-genre" class= "dropdown">
-                            <option selected>Genre</option>
-                            </select>
-                        <button type="button" id="custom-dropdown">
-                            <img src="../../../public/img/dropdown_button.svg" alt="">
-                        </button>
-                    </div>
-                    <div class = "filter">
-                        <select name="filter-artist" id="filter-artist" class="dropdown">
-                            <option selected>Artist</option>
-                            
-                        </select>
-                        <button type="button" id="custom-dropdown">
-                            <img src="../../../public/img/dropdown_button.svg" alt="">
-                        </button>
-                    </div>
+            <div class="search-sort">
+                <div class="search-box">
+                    <i class="bi bi-search"></i>
+                    <button type="submit"><img src="../../../public/img/search.svg" alt=""></button>
+                    <input id="searchInput" type="text" name = "song_title" placeholder="Search...">
                 </div>
-            </form>
+                <div class = "filter">
+                    <select name="filter-genre" id="filter-genre" class= "dropdown">
+                        <option selected>Genre</option>
+                        
+                        </select>
+                    <button type="button" id="custom-dropdown">
+                        <img src="../../../public/img/dropdown_button.svg" alt="">
+                    </button>
+                </div>
+                <div class = "filter">
+                    <select name="filter-artist" id="filter-artist" class="dropdown">
+                        <option selected>Artist</option>
+                    
+                    </select>
+                    <button type="button" id="custom-dropdown">
+                        <img src="../../../public/img/dropdown_button.svg" alt="">
+                    </button>
+                </div>
+            </div>
+        </form>
         </div>
         <div class="user">
             <i class="bi bi-person" onclick="profile(event)"></i>
         </div>
     </div>
-    <div class="main">
-        
+    <style>
+    form label {
+        color: white;
+        overflow: auto;
+    }
+    </style>
+    <div class="main" style="overflow: auto;">
+        <form action="/public/subs/accept" method="POST">
+            <h2 style="color: white;">Accept Subscription</h2>
+            <label for="creator_id">Creator ID:</label>
+            <input type="text" name="creator_id" id="creator_id"><br>
+            <label for="subscriber_id">Subscriber ID:</label>
+            <input type="text" name="subscriber_id" id="subscriber_id"><br>
+            <input type="submit" value="Accept Subscription">
+        </form>
+
+        <form action="/public/subs/getAll" method="POST">
+            <h2 style="color: white;">Get All Subscriptions</h2>
+            <input type="hidden" name="api_key" value="PHP_API_KEY">
+            <input type="submit" value="Get All Subscriptions">
+            
+        </form>
+
+        <form action="/public/subs/getAllSubsByIds" method="POST">
+            <h2 style="color: white;">Get Subscriptions by Subscriber ID</h2>
+            <label for="subscriber_id">Subscriber ID:</label>
+            <input type="text" name="subscriber_id" id="subscriber_id"><br>
+            <input type="hidden" name="api_key" value="PHP_API_KEY">
+            <input type="submit" value="Get Subscriptions by Subscriber ID">
+        </form>
+
+        <form action="/public/subs/reject" method="POST">
+            <h2 style="color: white;">Reject Subscription</h2>
+            <label for="creator_id">Creator ID:</label>
+            <input type="text" name="creator_id" id="creator_id"><br>
+            <label for="subscriber_id">Subscriber ID:</label>
+            <input type="text" name="subscriber_id" id="subscriber_id"><br>
+            <input type="submit" value="Reject Subscription">
+        </form>
+
+        <form action="/public/subs/insert" method="POST">
+            <h2 style="color: white;">Insert Subscription</h2>
+            <label for="creator_id">Creator ID:</label>
+            <input type="text" name="creator_id" id="creator_id"><br>
+            <label for="subscriber_id">Subscriber ID:</label>
+            <input type="text" name="subscriber_id" id="subscriber_id"><br>
+            <input type="submit" value="Insert Subscription">
+        </form>
+
+        <h2 style="color: white;">SOAP API Response:</h2>
+        <h2 style = "color: white">
+            <? var_dump($data)  ?>
+        </h2>
     </div>
-    <div class="play">
     
-    </div>
+    <div class="play"></div>
     <script src="/public/javascript/functional.js"></script>
     <script src="/public/javascript/openPopUp.js"></script>
     <script src="/public/javascript/search.js"></script>
     <script src="/public/javascript/sort.js"></script>
     <script src="/public/javascript/logout.js"></script>
-
 </body>
 </html>
